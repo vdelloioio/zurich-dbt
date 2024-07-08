1b4ff021cd2e9f1b61183d77fdc5f89aa5403d80
vincenzo.delloioio@emeal.nttdata.com <vincenzo.delloioio@emeal.nttdata.com>
2024-07-08 09:54:26 +0000

    primo test dbt cloud

---
+++ models/silver/ordini_ripulita.sql
@@ -0,0 +1,13 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key=['id_ordine']
+    )
+}}
+
+select *
+from `catalog-zurich-01`.bronze.ordini_validated
+where id_ordine is not null
+    and id_ordine > '0'
+    and id_persona is not null
+    and id_persona > '0'
