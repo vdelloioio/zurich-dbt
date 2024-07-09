d2725d4cf6043b91a9b68bd7a1e316c8dbae1e61
vincenzo dello ioio <vincenzo.delloioio@emeal.nttdata.com>
2024-07-09 09:34:56 +0000

    bug fix incremental

--- models/silver/ordini_ripulita.sql
+++ models/silver/ordini_ripulita.sql
@@ -1,5 +1,5 @@
-select *
-from `catalog-zurich-01`.bronze.ordini_validated
+select distinct *
+from `catalog-zurich-01`.bronze.ordini_validated_nodlt
 where id_ordine is not null
     and id_ordine > '0'
     and id_persona is not null
